35116417926a71fad83f966791a035c4c82cf33a
Mqueen15 <89357316+Mqueen15@users.noreply.github.com>
2021-08-30 21:10:05 +0200

    Delete find_max.py

--- python/find_max.py
+++
@@ -1,6 +0,0 @@
-def find_max(nums):
-    max_num = float ("-inf") # smaller than all other numbers
-    for num in nums:
-        if num > max_num:
-            # (fill in the missing line here)
-    return max_num
