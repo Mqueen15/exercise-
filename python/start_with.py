8af1b4557ace26e63ba3b07cf10def666b817e8c
Mqueen15 <89357316+Mqueen15@users.noreply.github.com>
2021-08-31 22:33:22 +0200

    Add files via upload

---
+++ python/start_with.py
@@ -0,0 +1,17 @@
+def start_K(l):
+    if l[0] == "K":
+        return True
+    else:
+        return False
+
+# A function call like this should return True:
+print(start_K("Kelly"))
+
+# And one like this should return False:
+print(start_K("Abe"))
+
+# And one like this should return False:
+print(start_K("Maram"))
+
+# And one like this should return True:
+print(start_K("Katy")) 
